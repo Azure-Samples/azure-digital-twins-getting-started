3d1c0a5e35b1886cf38577cfdb8b01abf302b454
Dan Hellem <dan_hellem@hotmail.com>
2021-07-27 10:45:39 -0700

    sample functions

---
+++ azure-functions/twin-updates/Dto/Room.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Amqp.Framing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwinUpdatesSample.Dto
+{
+    public class Room
+    {
+        public string id { get; set; }
+        public double temperature { get; set; } = -99;
+        public double humidity { get; set; } = -99;
+    }
+}
