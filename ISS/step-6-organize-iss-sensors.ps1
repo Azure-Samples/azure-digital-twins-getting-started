feeb148e1bbb32f6db87b14e44072eb6c5f466fa
Alex Hayward <haywardalex@microsoft.com>
2022-03-28 15:20:52 +0100

    displicplie organization psuedo code

--- ISS/step-6-organize-iss-sensors.ps1
+++ ISS/step-6-organize-iss-sensors.ps1
@@ -1,11 +1,19 @@
 Param([string] $iss_adt_instance_name, [string] $ResourceGroupName)
 
-Install-Module -Name Az.DigitalTwins
-
 $iss_sensors = $(az dt twin query -n $iss_adt_instance_name -g $ResourceGroupName -q "SELECT * FROM DigitalTwins WHERE MessageType = 'IssSensor'" ) | ConvertFrom-Object 
 
 Write-Output $iss_sensors
 
-foreach ($twin in $iss_sensors) {
-    Write-Output 'Hello'
+foreach ($twinList in $iss_sensors) {
+    
+    ## get the discipline of the sensor 
+
+    ## split by / (a sensor can be in muplitple disciplines)
+
+    ## for each discipline
+
+        ## create disicipline twin if not exists
+
+        ## add sensor to disicpline using discipline "HasMember Relationship"
+
 }
