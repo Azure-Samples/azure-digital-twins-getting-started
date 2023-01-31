0690c3506f92214e85b243a4e5bc764908692763
Abhinav Ghai <abhinavghai12290@gmail.com>
2023-01-31 10:06:07 -0800

    Address comments

--- bulk-import/ndjson-generator/NDJsonGenerator.cs
+++ bulk-import/ndjson-generator/NDJsonGenerator.cs
@@ -1,63 +1,58 @@
-﻿namespace NDJsonGenerator
+﻿namespace Azure.DigitalTwins.Utilities
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Text;
     using System.Text.Json;
+    using Azure.DigitalTwins.Core;
     using Azure.DigitalTwins.Core.Serialization;
 
-    public class NDJsonGenerator
+    public sealed class NDJsonGenerator
     {
-        private const string HeaderSection = "{\"Section\": \"Header\"}";
-        private const string ModelsSection = "{\"Section\": \"Models\"}";
-        private const string TwinsSection = "{\"Section\": \"Twins\"}";
-        private const string RelationshipsSection = "{\"Section\": \"Relationships\"}";
-        private const string DefaultHeader = "{\"fileVersion\": \"1.0.0\", \"author\": \"authorName\", \"organization\": \"organization\"}";
+        private const string headerSection = "{\"Section\": \"Header\"}";
+        private const string modelsSection = "{\"Section\": \"Models\"}";
+        private const string twinsSection = "{\"Section\": \"Twins\"}";
+        private const string relationshipsSection = "{\"Section\": \"Relationships\"}";
+        private const string defaultHeader = "{\"fileVersion\": \"1.0.0\", \"author\": \"authorName\", \"organization\": \"organization\"}";
 
         public static Stream GenerateNDJson(IEnumerable<string> models, IEnumerable<BasicDigitalTwin> twins, IEnumerable<BasicRelationship> relationships, string header = "")
         {
             var stream = new MemoryStream();
             var writer = new StreamWriter(stream, Encoding.UTF8);
 
-            writer.WriteLine(HeaderSection);
-            if (String.IsNullOrEmpty(header)) writer.WriteLine(DefaultHeader);
-            else writer.WriteLine(header);
+            writer.WriteLine(headerSection);
+            writer.WriteLine(string.IsNullOrEmpty(header) ? defaultHeader : header);
 
-            if (models != null)
+            models ??= Enumerable.Empty<string>();
+            writer.WriteLine(modelsSection);
+            foreach (var model in models)
             {
-                writer.WriteLine(ModelsSection);
-                foreach (var model in models)
-                {
-                    var sanitizedModel = model.Replace(Environment.NewLine, String.Empty);
-                    writer.WriteLine(sanitizedModel);
-                }
+                var sanitizedModel = model.Replace(Environment.NewLine, string.Empty);
+                writer.WriteLine(sanitizedModel);
             }
 
-            if (twins != null)
+            twins ??= Enumerable.Empty<BasicDigitalTwin>();
+            writer.WriteLine(twinsSection);
+            foreach (var twin in twins)
             {
-                writer.WriteLine(TwinsSection);
-                foreach (var twin in twins)
-                {
-                    string sanitizedTwin = JsonSerializer.Serialize(twin, twin.GetType()).Replace(Environment.NewLine, String.Empty);
-                    writer.WriteLine(sanitizedTwin);
-                }
+                string sanitizedTwin = JsonSerializer.Serialize(twin, twin.GetType()).Replace(Environment.NewLine, string.Empty);
+                writer.WriteLine(sanitizedTwin);
             }
 
-            if (relationships != null)
+            relationships ??= Enumerable.Empty<BasicRelationship>();
+            writer.WriteLine(relationshipsSection);
+            foreach (var relationship in relationships)
             {
-                writer.WriteLine(RelationshipsSection);
-                foreach (var relationship in relationships)
-                {
-                    string sanitzedRelationship = JsonSerializer.Serialize(relationship, relationship.GetType()).Replace(Environment.NewLine, String.Empty);
-                    writer.WriteLine(sanitzedRelationship);
-                }
+                string sanitzedRelationship = JsonSerializer.Serialize(relationship, relationship.GetType()).Replace(Environment.NewLine, string.Empty);
+                writer.WriteLine(sanitzedRelationship);
             }
 
-            writer.WriteLine(); // Bug: BulkAPI requires newline at the end of the ndjson file
+            writer.WriteLine();
 
             writer.Flush();
-            stream.Seek(0, SeekOrigin.Begin); // Set seek head to begining of stream to return ready to read stream
+            stream.Seek(0, SeekOrigin.Begin);
 
             return stream;
         }
--- bulk-import/ndjson-generator/Program.cs
+++ bulk-import/ndjson-generator/Program.cs
@@ -1,34 +1,24 @@
-﻿namespace NDJsonGenerator
+﻿namespace Azure.DigitalTwins.Utilities
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
-    using Azure.DigitalTwins.Core.Serialization;
-    using Helpers;
+    using Azure.DigitalTwins.Core;
+    using Azure.DigitalTwins.Utilities.Helpers;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            // Example usage
-            Stream ndjsonStream = null;
-            try
-            {
-                string header = "{\"fileVersion\": \"1.0.0\", \"author\": \"foobar\", \"organization\": \"contoso\"}";
-                IEnumerable<string> models = DataGenerator.GenerateModels(10);
-                IEnumerable<BasicDigitalTwin> twins = DataGenerator.GenerateTwins(10, 100);
-                IEnumerable<BasicRelationship> relationships = DataGenerator.GenerateRelationships(100);
+            string header = "{\"fileVersion\": \"1.0.0\", \"author\": \"foobar\", \"organization\": \"contoso\"}";
+            IEnumerable<string> models = RandomNDJsonDataGenerator.GenerateModels(10);
+            IEnumerable<BasicDigitalTwin> twins = RandomNDJsonDataGenerator.GenerateTwins(10, 100);
+            IEnumerable<BasicRelationship> relationships = RandomNDJsonDataGenerator.GenerateRelationships(100);
 
-                ndjsonStream = NDJsonGenerator.GenerateNDJson(models, twins, relationships, header);
+            using (Stream ndjsonStream = NDJsonGenerator.GenerateNDJson(models, twins, relationships, header))
+            {
                 Console.WriteLine(new StreamReader(ndjsonStream).ReadToEnd());
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            finally
-            {
-                if (ndjsonStream != null) ndjsonStream.Close();
+                Console.ReadLine();
             }
         }
     }
